8bfc19bb7a0a350d2618b0c3b3cfc4caed93a712
Neel Sukhadia <neelsukhadia@gmail.com>
2013-11-20 15:26:26 -0600

    Revert "Modified to use require.js"
    This reverts commit 55d3e4a3a32fa127c4e2bd2e6be41503ec93eed7.

--- public/js/app.js
+++
@@ -1,9 +0,0 @@
-
-define(['backbone', 'router'], function(Backbone, AppRouter) {
-    return {
-    	init: function() {
-    		app = new AppRouter();
-    	    Backbone.history.start();
-    	}
-    }
-});
