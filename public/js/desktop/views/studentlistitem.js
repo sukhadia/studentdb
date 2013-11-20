d05b0d53d6c5f5466d270914835093e1e6e6279c
unknown <nsukhadia@CV11714.corp.classifiedventures.com>
2013-11-20 15:03:49 -0600

    Revert "Modified to use require.js"
    This reverts commit 55d3e4a3a32fa127c4e2bd2e6be41503ec93eed7.

--- public/js/desktop/views/studentlistitem.js
+++
@@ -1,9 +0,0 @@
-define(['common/views/studentlistitembase', 'text!desktop/tpl/StudentListItemView.html'], function(StudentListItemViewBase, template) {	
-	return StudentListItemViewBase.extend({
-	    tagName:  'tr',
-
-	    getTemplate: function() {
-	    	return template;
-	    }
-	});
-});
--- public/js/mobile/views/studentlistitem.js
+++
@@ -1,9 +0,0 @@
-define(['common/views/studentlistitembase', 'text!mobile/tpl/StudentListItemView.html'], function(StudentListItemViewBase, template) {	
-	return StudentListItemViewBase.extend({
-	    tagName:  'li',
-
-	    getTemplate: function() {
-	    	return template;
-	    }
-	});
-});
